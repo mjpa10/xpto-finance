aaf44afa88f893edc2441c8fa06a38a2dfd972ea
mtheu <m.theus.jose.pereira@gmail.com>
2026-07-20 14:10:33 -0300

    Inicializando Projeto utilizando Spring Boot

---
+++ src/main/java/br/com/matheus/xpto_finance/XptoFinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.xpto_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptoFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XptoFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheus/xpto_finance/XptoFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.xpto_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XptoFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
